9d11a149379e03746a18bf03c36d89dd11b66565
Robert Kang <robertkang96@gmail.com>
2018-12-10 14:24:02 -0600

    Put the Node class into SkipList

--- SkipList.java
+++ SkipList.java
@@ -1,11 +1,46 @@
 import java.util.Random;
 import java.util.*;
 
+class Node {
+
+	public int levels;
+	public int key;
+	public int data;
+	public Node[] next;
+	public boolean isNull;
+	public boolean isHeader;
+	
+	Node(int levels, int key, int data) {	// Constructor to make nodes after the header
+		this.levels = levels;
+		this.key = key;
+		this.data = data;
+		next = new Node[levels];
+		isHeader = false;
+		isNull = false;
+	}
+	
+	Node(String str){
+		if(str.equals("null")){
+			isNull = true;
+			next = null;
+		}
+		else if(str.equals("header")) {
+			isHeader = true;
+			next = new Node[1];
+		}
+	}
+	
+	 @Override
+	 public String toString() {
+		 return "(" + key + "," + data + ")";
+	 }
+}
+
 public class SkipList {
 	
 	public static Node[] list;
 	public static int currentLevels;
-	private final static int maxLevels = 6;
+	private final static int maxLevels = 11;
 	
 
 	public static void initializeList() {
@@ -100,7 +135,7 @@ public class SkipList {
 			return pos.next[0].data;
 		}
 		else {
-			System.out.println("Given key does not exist.");
+			System.out.println(0);
 			return 0;
 		}
 	}
